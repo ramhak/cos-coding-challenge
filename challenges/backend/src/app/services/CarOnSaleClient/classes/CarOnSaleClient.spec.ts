60daa2ff3d96d70b17072e4001b6c065b071c1f2
Ramin Hakimi <ramin.hakimi@outlook.com>
2021-11-23 14:26:13 +0330

    refactor the test code.it is now more readable and maintainable.

--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.spec.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.spec.ts
@@ -4,11 +4,11 @@ import {Container} from "inversify";
 import {DependencyIdentifier} from "../../../DependencyIdentifiers";
 import {CarOnSaleClient} from "./CarOnSaleClient";
 import 'reflect-metadata';
-import {ICarOnSaleApi, ISalesmanAllBiddingDataResult} from "../../CarOnSaleApi/interface/ICarOnSaleApi";
+import {CarOnSaleClientBuilder} from "./TestDSL";
 
 describe("get running auctions", () => {
     let container: Container;
-    let api: ICarOnSaleApi;
+
     beforeEach(() => {
         container = new Container();
         container.bind(DependencyIdentifier.CAR_ON_SALE_CLIENT).to(CarOnSaleClient)
@@ -16,38 +16,37 @@ describe("get running auctions", () => {
 
     describe("number of auctions", () => {
         it("should be 0 when there are no auctions", async () => {
-            api = new CarOnSaleApiStub([]);
-            container.bind(DependencyIdentifier.CAR_ON_SALE_API).toConstantValue(api);
-            const client = container.get<ICarOnSaleClient>(DependencyIdentifier.CAR_ON_SALE_CLIENT);
+            const client = new CarOnSaleClientBuilder(container)
+                .build()
             expect((await client.getRunningAuctions()).numberOfRunningAuctions).eq(0);
         });
         it("should return the number of running auctions", async () => {
-            api = new CarOnSaleApiStub([{numBids:0}, {numBids:0}]);
-            container.bind(DependencyIdentifier.CAR_ON_SALE_API).toConstantValue(api);
-            const client = container.get<ICarOnSaleClient>(DependencyIdentifier.CAR_ON_SALE_CLIENT);
+            const client =new CarOnSaleClientBuilder(container)
+                .anAuction()
+                .buildAuction()
+                .anAuction()
+                .buildAuction()
+                .build()
             expect((await client.getRunningAuctions()).numberOfRunningAuctions).eq(2);
 
         });
     });
-    describe("the auctions",()=>{
-        it("should return number of bids for each auction",async ()=>{
-            api = new CarOnSaleApiStub([{numBids:1},{numBids:2}]);
-            container.bind(DependencyIdentifier.CAR_ON_SALE_API).toConstantValue(api);
+    describe("the auctions", () => {
+        it("should return number of bids for each auction", async () => {
+            new CarOnSaleClientBuilder(container)
+                .anAuction()
+                .withNumBids(1)
+                .buildAuction()
+                .anAuction()
+                .withNumBids(2)
+                .buildAuction()
+                .build();
             const client = container.get<ICarOnSaleClient>(DependencyIdentifier.CAR_ON_SALE_CLIENT);
             let runningAuctions = await client.getRunningAuctions();
             expect(runningAuctions.auctions[0].numberOfBids).eq(1)
             expect(runningAuctions.auctions[1].numberOfBids).eq(2)
         })
+
     })
 
 });
-
-class CarOnSaleApiStub implements ICarOnSaleApi {
-
-    constructor(private data: ISalesmanAllBiddingDataResult[]) {
-    }
-
-    getSalesmanAllBiddingData(_userId: string): Promise<ISalesmanAllBiddingDataResult[]> {
-        return Promise.resolve(this.data);
-    }
-}
---
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/TestDSL.ts
@@ -0,0 +1,58 @@
+import {ICarOnSaleApi, ISalesmanAllBiddingDataResult} from "../../CarOnSaleApi/interface/ICarOnSaleApi";
+import {Container} from "inversify";
+import {DependencyIdentifier} from "../../../DependencyIdentifiers";
+import {ICarOnSaleClient} from "../interface/ICarOnSaleClient";
+
+export class CarOnSaleApiStub implements ICarOnSaleApi {
+    constructor(private data: ISalesmanAllBiddingDataResult[]) {
+    }
+
+    getSalesmanAllBiddingData(_userId: string): Promise<ISalesmanAllBiddingDataResult[]> {
+        return Promise.resolve(this.data);
+    }
+}
+
+export class CarOnSaleClientBuilder {
+
+    constructor(private container: Container, private auctions: ISalesmanAllBiddingDataResult[] = []) {
+        this.auctions = auctions;
+        this.container = container;
+    }
+
+    anAuction() {
+        return new AuctionBuilder(this);
+    };
+
+    addAuction(auction: ISalesmanAllBiddingDataResult) {
+        this.auctions.push(auction);
+    }
+
+    build(): ICarOnSaleClient {
+        let carOnSaleApiStub = new CarOnSaleApiStub(this.auctions);
+        this.container.bind(DependencyIdentifier.CAR_ON_SALE_API).toConstantValue(carOnSaleApiStub)
+        // this.container.bind(DependencyIdentifier.CAR_ON_SALE_API).toConstantValue(carOnSaleApiStub)
+        return this.container.get(DependencyIdentifier.CAR_ON_SALE_CLIENT);
+    }
+}
+
+export class AuctionBuilder {
+    private auction: ISalesmanAllBiddingDataResult;
+
+
+    constructor(private p: CarOnSaleClientBuilder) {
+        this.auction = {
+            numBids: 0
+        };
+    }
+
+    withNumBids(numBids: number): AuctionBuilder {
+        this.auction.numBids = numBids;
+        return this;
+    }
+
+    buildAuction(): CarOnSaleClientBuilder {
+        this.p.addAuction(this.auction)
+        return this.p;
+    }
+
+}
